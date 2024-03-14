32909e344fb4a418a69bfaf6b7df883e7a337f91
Ruslan <axelezgames@gmail.com>
2024-03-15 00:20:12 +0500

    feat(*): initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="grid grid-cols-2">
+        <MyComponents />
+        <MyComponents />
+      </div>
+      <div className="grid grid-cols-3">
+        <MyComponents />
+        <MyComponents />
+        <MyComponents />
+      </div>
+      <div className="grid grid-cols-2">
+        <MyComponents />
+        <MyComponents />
+      </div>
+    </>
+  );
 }
 
 export default App;

128cf4cae85ce16ede19585c04e3d465cb86e3e9
Ruslan <axelezgames@gmail.com>
2024-06-03 14:20:07 +0500

    feat(url switch): added dynamic url picker

--- client/src/hooks/useImageGenerate.tsx
+++ client/src/hooks/useImageGenerate.tsx
@@ -5,6 +5,8 @@ import { setKeepImage } from "../redux/slices/imageKeeperSlice";
 import { useAppSelector } from "../types/reduxHooks";
 import { useAppDispatch } from "../types/reduxHooks";
 
+import determineEndpoint from "@/utils/endpointSwitch";
+
 const useImageGenerate = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
@@ -12,13 +14,15 @@ const useImageGenerate = () => {
   const dispatch = useAppDispatch();
   const settings = useAppSelector((data) => data.settings);
 
-  const url = "http://127.0.0.1:8000/scratchToImg";
+  const url = "http://127.0.0.1:8000";
 
   const getPostImage = async () => {
     setIsLoading(true);
 
+    const endpoint = determineEndpoint(settings, url);
+
     try {
-      const response = await fetch(url, {
+      const response = await fetch(endpoint, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
---
+++ client/src/utils/endpointSwitch.ts
@@ -0,0 +1,20 @@
+import { TPromptSchema } from "@/types/modelsTypes";
+
+export default function determineEndpoint(
+  settings: TPromptSchema,
+  url: string
+): string {
+  const auto = settings.auto;
+  const input_image = settings.alwayson_scripts.controlnet.args[0].input_image;
+
+  switch (true) {
+    case input_image && auto:
+      return `${url}/auto/controlnet`;
+
+    case input_image && !auto:
+      return `${url}/default/controlnet`;
+
+    default:
+      return `${url}/default`;
+  }
+}

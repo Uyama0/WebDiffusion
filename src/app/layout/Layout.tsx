1490f63b15086c147ba626b3f4c29e9a9afa1648
Uyama0 <axelezgames@gmail.com>
2025-03-15 20:39:30 +0300

    test

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router";
 
-import { Header } from "@/widgets/header";
+// import { Header } from "@/widgets/header";
 
 import styles from "./styles.module.css";
 
 export const Layout: React.FC = () => {
   return (
     <div className={styles.layout_wrapper}>
-      <Header />
+      {/* <Header /> */}
       <main className={styles.main}>
         <Outlet />
       </main>

6c96bc89d5895469591ebc488c7b9d8915ba6d47
Uyama0 <axelezgames@gmail.com>
2025-06-28 20:21:06 +0300

    feat(dialog): add dialog component

--- client/src/components/ImageViewer/index.tsx
+++ client/src/components/ImageViewer/index.tsx
@@ -1,24 +1,25 @@
 import { useState, useEffect } from 'react';
 
 import { ImageIcon } from '@radix-ui/react-icons';
-
 import { useAppSelector } from '@/hooks';
-
 import { imagesSelector } from '@/redux/selectors/images';
 
 export const ImageViewer = () => {
     const [image, setImage] = useState('');
     const images = useAppSelector(imagesSelector);
 
+    console.log(images);
+
     useEffect(() => {
         setImage(images[images.length - 1]);
+        setImage(images?.images[0]);
     }, [images]);
 
     return (
         <section className='border-4 border-border rounded-md flex justify-center items-center h-[50vh]'>
             {image ? (
                 <img
-                    src={image}
+                    src={images}
                     alt='input image'
                     className='w-full h-full object-contain rounded-base-border-radius'
                 />

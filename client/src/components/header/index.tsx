681672ed472d89e685c2cfb07980d3e8c2a15c90
unknown <Uyama>
2025-07-09 12:53:13 +0500

    fix

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = () => {
     return (
         <div className='px-lg border-b border-border'>
             <header className='flex py-sm px-sm items-center justify-between border-x border-border relative'>
-                <span className='text-lg font-semibold'>NeuroSketch</span>
+                <span className='text-lg font-bold'>NeuroSketch</span>
                 <Navigation />
                 <div className='flex gap-sm'>
                     <Button variant='outline' size='icon'>
